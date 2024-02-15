3de1c0cace7f9ad1be663361977c427bc0278413
simon-sahlin-nti-johanneberg <simonsahlin@somin.se>
2024-02-15 20:09:53 +0100

    Add one operator functionality

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 let input1 = 15;
 let input2 = 5;
-let operator = "+";
+let operator = "";
 
+let displayValue = "";
+let hiddenValue = "";
+let displayText = document.querySelector("#displayText")
+displayText.textContent = "abo";
+
+for (let i = 0; i < 10; i++) {
+    let numButton = document.querySelector("#btn" + i);
+    numButton.addEventListener("click", () => PressNumber(i));
+}
+
+function PressNumber(index){
+    displayValue += ""+index;
+    displayText.textContent = displayValue;
+}
+
+let addButton = document.querySelector("#btnPlus");
+addButton.addEventListener("click", () => PressOperator("+"))
+let minusButton = document.querySelector("#btnMinus");
+minusButton.addEventListener("click", () => PressOperator("-"))
+let multiplyButton = document.querySelector("#btnMutliply");
+multiplyButton.addEventListener("click", () => PressOperator("*"))
+let divideButton = document.querySelector("#btnDivide");
+divideButton.addEventListener("click", () => PressOperator("/"))
+
+function PressOperator(index){
+    operator = index;
+    hiddenValue = displayValue;
+    displayValue = "";
+}
+
+let equalsButton = document.querySelector("#btnEquals");
+equalsButton.addEventListener("click", () => PressEquals());
+
+function PressEquals(){
+    input1 = +hiddenValue;
+    input2 = +displayValue;
+    displayValue = Operate();
+    displayText.textContent = displayValue;
+
+}
 
 function Add(num1, num2){
     return num1 + num2;
@@ -39,5 +79,3 @@ function Operate(){
     }
     return result
 }
-
-console.log(Operate());
